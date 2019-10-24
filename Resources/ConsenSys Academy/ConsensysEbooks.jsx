ff3c9199700229bc37330e3f44a70f8290560186
robbie <robbiekruszynski@gmail.com>
2019-10-24 10:48:12 -0700

    add info to ebook

--- Resources/ConsenSys Academy/ConsensysEbooks.jsx
+++ Resources/ConsenSys Academy/ConsensysEbooks.jsx
@@ -1,7 +1,7 @@
 ---
 template: resource
 url: 
-alias: 
+alias: E-bboks
 title: E-books
 tagline: Blockchain and Ethereum Training
 summary:  Description: 
@@ -9,3 +9,20 @@ image:
 imageShowcase: https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1993&q=80
 date: 2019-01-29
 ---
+
+title: Blockchain Basics
+url: https://consensys.net/academy/blockchain-basics-book/
+summary:  Description:  This guide is designed for anyone who is looking to wrap their head around blockchain technology, how it works, and how it could affect the world around them.
+
+title: Kauri - Getting Started
+url: https://kauri.io/collection/5cb55c871325f2000141df73/getting-started
+tagline: Learn about blockchains, smart contracts, Ethereum, and what you can build with them
+summary:  Description: Kauri is a portal where project teams and individuals can engage, generate, and curate high quality and up-to-date technical resources, committed to enabling the community to curate the decentralized knowledge base and support network that the Ethereum ecosystem needs to reach its full potential. Kauri provides both new and experienced developers with completely free access to a curated, high-quality, and continuously expanding Ethereum knowledge base
+
+title: Java Ethereum Community
+url: https://kauri.io/community/5d2f30daaba2920001c82409
+summary:  Description: If you are a Java Developer, take tutorials and work on projects here
+
+title: Interested in the up-and-coming world of decentralized finance?
+url: https://kauri.io/collection/5cab3a8c4e04590001eccfa2/open-finance-(defi)
+summary:  Description: Permissionless, open access financial products and protocols built using peer-to-peer technology like the Ethereum blockchain
