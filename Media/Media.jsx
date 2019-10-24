37e3951e5ba134a5402c9df470b5b99f44b24274
robbie <robbiekruszynski@gmail.com>
2019-10-24 12:28:36 -0700

    add media links

---
+++ Media/Media.jsx
@@ -0,0 +1,35 @@
+---
+template: media
+url: 
+alias: Media Coverage
+title: Media Coverage 
+tagline: 1 Million Developers Media Coverage
+summary: 
+image: 
+imageShowcase: 
+date: 2019-01-29
+---
+
+title: Lubin Speech
+summary: The call to action
+url: https://www.youtube.com/watch?v=Z1FBRy2iVp8&t=1s
+
+title:Dapp University
+summary: 1,000,000 Ethereum Developers by 2020!?
+url: https://www.youtube.com/watch?v=Ajcs8hCaZdg
+
+title: Consensys Coverage
+summary: One Million Ethereum Developers: Five ConsenSys Announcements from Devcon 5
+url: https://media.consensys.net/one-million-ethereum-developers-five-consensys-announcements-from-devcon-5-28520ce63d73
+
+title:Ethereum World News
+summary: ConsenSys Chief Lubin Wants One Million Ethereum Developers… STAT
+url: https://en.ethereumworldnews.com/consensys-lubin-one-mil-ethereum-devs/
+
+title:Cryptomode
+summary: Can Ethereum Really Attract one Million Active Developers?
+url: https://cryptomode.com/can-ethereum-really-attract-one-million-active-developers/
+
+title:ZyCrypto
+summary:Ethereum Is Gearing Up For One Million Developers; What Are The Possibilities?
+url: https://zycrypto.com/ethereum-is-gearing-up-for-one-million-developers-what-are-the-possibilities/
