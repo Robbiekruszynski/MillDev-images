5ff29016fc8a08dcc8602179683a4c1dda0a735e
robbie <robbiekruszynski@gmail.com>
2019-10-24 10:09:32 -0700

    add title / url to webinars

---
+++ ConsensysWebinars.jsx
@@ -0,0 +1,34 @@
+---
+template: resource
+url: https://learn.consensys.net/catalog
+alias: Webinars
+title: Webinars
+tagline: Blockchain and Ethereum Training
+summary:  Description: Academy hosts free webinars for anyone to view, and are updated with new topics on a regular basis
+image: 
+imageShowcase:
+date: 2019-01-29
+---
+title: The Tokenized Future
+url: https://learn.consensys.net/catalog/info/id:174
+
+title: Stablecoins
+url: https://learn.consensys.net/catalog/info/id:177
+
+title: Web 3 Career Webinar
+url: https://learn.consensys.net/catalog/info/id:173
+
+title: Preparing for a Smart Contract Audit with ConsenSys Diligence
+url: https://learn.consensys.net/catalog/info/id:172
+
+title: The Future of Organizing with the Bounties Network
+url: https://learn.consensys.net/catalog/info/id:171
+
+title: Network Data Analytics/Reporting with Alethio
+url: https://learn.consensys.net/catalog/info/id:166
+
+title: Smart Contracts Off-Chain with Rhombus
+url: https://learn.consensys.net/catalog/info/id:164
+
+title: Libra: Building and Understanding Facebook’s Cryptocurrency
+url: https://learn.consensys.net/catalog/info/id:169
