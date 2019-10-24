bc08f17ac170ceba95f0d79dc9d668accf281d2f
robbie <robbiekruszynski@gmail.com>
2019-10-24 09:58:11 -0700

    add files

---
+++ Resources/ConsensysCertifications.jsx
@@ -0,0 +1,11 @@
+---
+template: resource
+url: 
+alias: 
+title: 
+tagline: Blockchain and Ethereum Training
+summary:  Description: 
+image: 
+imageShowcase:
+date: 2019-01-29
+---
---
+++ Resources/ConsensysCourses.jsx
@@ -0,0 +1,11 @@
+---
+template: resource
+url: https://learn.consensys.net/catalog/info/id:159
+alias: Blockchain Developer Online Bootcamp Registration
+title: Blockchain Developer Online Bootcamp Registration:
+tagline: Blockchain and Ethereum Training
+summary:  Description: Guides, resources, and tools for developers building on Ethereum.
+image: https://i.imgur.com/ENm32of.png
+imageShowcase: https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1993&q=80
+date: 2019-01-29
+---
