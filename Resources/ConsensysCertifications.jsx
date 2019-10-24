da8df3c765f8aa6d8a0448e42f00bdb916e8b106
robbie <robbiekruszynski@gmail.com>
2019-10-24 10:17:38 -0700

    add work to jsx files linking

--- Resources/ConsensysCertifications.jsx
+++ Resources/ConsensysCertifications.jsx
@@ -1,11 +1,26 @@
 ---
 template: resource
-url: 
-alias: 
-title: 
+url: https://learn.consensys.net/catalog
+alias: Consensys Certifications
+title: Consensys Certifications
 tagline: Blockchain and Ethereum Training
-summary:  Description: 
+summary:  Description: There are a suite of certifications for those looking to demonstrate their understanding of Ethereum
 image: 
 imageShowcase:
 date: 2019-01-29
 ---
+
+title: Ethereum Business Professional Certification Exam
+url: https://learn.consensys.net/catalog/info/id:154
+
+title: Ethereum Developer Certification Exam
+url: https://learn.consensys.net/catalog/info/id:131
+
+title: Enterprise Ethereum Developer Certification Exam
+url: https://learn.consensys.net/catalog/info/id:142
+
+title: Smart Contract Developer Certification Exam
+url: https://learn.consensys.net/catalog/info/id:129
+
+title: Hyperledger Besu Essentials Certification
+url: https://learn.consensys.net/catalog/info/id:151
--- Resources/ConsensysCourses.jsx
+++ Resources/ConsensysCourses.jsx
@@ -1,11 +1,26 @@
 ---
 template: resource
-url: https://learn.consensys.net/catalog/info/id:159
-alias: Blockchain Developer Online Bootcamp Registration
-title: Blockchain Developer Online Bootcamp Registration:
+url: https://learn.consensys.net/catalog
+alias: Consensys Courses
+title: Consensys Courses
 tagline: Blockchain and Ethereum Training
-summary:  Description: Guides, resources, and tools for developers building on Ethereum.
+summary:  Description: Academy hosts courses to get web2 developers building dapps on Ethereum and understanding the tools and technologies that make up the ecosystem.
 image: https://i.imgur.com/ENm32of.png
 imageShowcase: https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1993&q=80
 date: 2019-01-29
 ---
+
+title: Blockchain Developer Program Bootcamp
+url: https://learn.consensys.net/catalog/info/id:159
+
+title: Blockchain Developer Program
+url: https://learn.consensys.net/catalog/info/id:141
+
+title: Hyperledger Besu Essentials
+url: https://learn.consensys.net/catalog/info/id:144
+
+title: Libra: Building and Understanding Facebook’s Cryptocurrency
+url: https://learn.consensys.net/catalog/info/id:178
+
+title: Blockchain Essentials: An Introduction to Blockchain
+url: https://learn.consensys.net/catalog/info/id:168
