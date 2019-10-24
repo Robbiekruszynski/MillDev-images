ce50a34342e1e40c932db4c5fb8826397f9a98ec
robbie <robbiekruszynski@gmail.com>
2019-10-24 09:41:23 -0700

    add info to ethdevres

--- Resources/EthereumDeveloperResources.jsx
+++ Resources/EthereumDeveloperResources.jsx
@@ -5,7 +5,7 @@ alias: Ethereum.org Developer Resources
 title: Ethereum Developer Resources
 tagline: Blockchain and Ethereum Training
 summary:  Description: Guides, resources, and tools for developers building on Ethereum.
-image: https://i.imgur.com/C6JfnZl.png
+image: https://i.imgur.com/ENm32of.png
 imageShowcase: https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1993&q=80
 date: 2019-01-29
 ---
