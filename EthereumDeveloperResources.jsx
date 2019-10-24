2aac4e1750b9df8582a1aab667ff0b44bfaa7285
robbie <robbiekruszynski@gmail.com>
2019-10-24 09:32:14 -0700

    add jsx files from documentation

---
+++ EthereumDeveloperResources.jsx
@@ -0,0 +1,11 @@
+---
+template: resource
+url: https://www.ethereum.org/developers/#getting-started
+alias: Ethereum.org Developer Resources
+title: Ethereum Developer Resources
+tagline: Blockchain and Ethereum Training
+summary:  Description: Guides, resources, and tools for developers building on Ethereum.
+image: https://i.imgur.com/C6JfnZl.png
+imageShowcase: https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1993&q=80
+date: 2019-01-29
+---
--- Resources/ConsensysPortal.jsx
+++ Resources/ConsensysPortal.jsx
@@ -5,7 +5,7 @@ alias: Consensys Developer Portal
 title: Consensys Developer Portal
 tagline: Blockchain and Ethereum Training
 summary:  Everything you need to start building on the Ethereum blockchain.
-image: https://imgur.com/eEMQFv6.png
+image: https://i.imgur.com/C6JfnZl.png
 imageShowcase: https://images.unsplash.com/photo-1480506132288-68f7705954bd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1993&q=80
 date: 2019-01-29
 ---
