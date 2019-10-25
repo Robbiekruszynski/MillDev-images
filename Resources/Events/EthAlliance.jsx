e975ba22eb7d591539276a227f21a6e0ad954be9
robbie <robbiekruszynski@gmail.com>
2019-10-25 11:30:13 -0700

    change info

--- Resources/Events/EthAlliance.jsx
+++
@@ -1,8 +0,0 @@
----
-
-title: Blockchain-expo
-summary:UNLOCKING THE INTERNET OF VALUE
-url: https://blockchain-expo.com/northamerica/
-date: 2019-01-29
-
----
--- Resources/Events/Ethwaterloo.jsx
+++ Resources/Events/Ethwaterloo.jsx
@@ -2,6 +2,7 @@
 title: ETHGlobal's Premiere event of 2019
 summary: Back to Waterloo, where it all began
 url: https://ethwaterloo.com/
-date: 2019-01-29
+tagline: Location Waterloo, Ontario
+date: 2019-11-8, 2019-10-8
 
 ---
