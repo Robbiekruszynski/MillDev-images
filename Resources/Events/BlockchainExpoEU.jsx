91e524988e168a96232ab9e951bf650bfbab978d
robbie <robbiekruszynski@gmail.com>
2019-10-25 11:01:20 -0700

    fix routing with links

--- Resources/Events/BlockchainExpoEU.jsx
+++ Resources/Events/BlockchainExpoEU.jsx
@@ -1,7 +1,7 @@
 ---
 title: Blockchain Expo Europe 2020
 summary: Blockchain Expo Europe 2020
-url: https://blockchain-expo.com/global/
+url: hhttps://blockchain-expo.com/europe/
 tagline: Location Amsterdam
 date: 2020-07-1, 2020-07-2
 
--- Resources/Events/BlockchainExpoNA.jsx
+++ Resources/Events/BlockchainExpoNA.jsx
@@ -1,7 +1,7 @@
 ---
 
 title: Global Blockchain Expo North America
-url: https://blockchain-expo.com/global/
+url: https://blockchain-expo.com/northamerica/
 tagline: Location Santa Clara, CA
 date: 2019-11-13, 2019-11-14
 
